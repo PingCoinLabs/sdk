fddafcced3ec41a0991412096d29e722959d7640
Ping Coin <ordercrud@gmail.com>
2026-04-08 00:11:59 +0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>⚡ Ping Coin</h1>
+      <p>Lightning speed crypto for global adoption.</p>
+    </div>
+  );
+}
+
+export default App;
